403e0bc815e824fa1aede595de51a41aeb1e7c59
yurididomenico <yurididomenico93@gmail.com>
2022-10-18 13:55:24 +0200

    Chiedo il nome all'utente

--- assets/JavaScript/main.js
+++ assets/JavaScript/main.js
@@ -7,3 +7,28 @@ aggiungiamo alla fine un numero
 Infine scriviamo sulla pagina "la tua password insicurissima è: nomecognomecolorepreferito21"
 
 */
+
+/*
+    Struttura:
+    let nome = prompt("Nome: ");
+    console.log(nome);
+    document.getElementById('nome').innerHTML = `Il tuo nome è ${nome}`;
+*/
+
+
+/*
+-Chiedi all’utente il suo nome
+
+-Chiedi il suo cognome
+
+-Chiedi il suo colore preferito
+
+-Aggiungiamo alla fine un numero
+
+-Scriviamo sulla pagina "la tua password insicurissima è: nomecognomecolorepreferito21"
+
+*/
+
+let nome = prompt("Nome: ");
+console.log(nome);
+document.getElementById('nome').innerHTML = `Il tuo nome è ${nome}`;
