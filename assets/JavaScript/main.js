77cb9caea8e570016dcf5e9ed885118a79407824
yurididomenico <yurididomenico93@gmail.com>
2022-10-18 13:58:52 +0200

    chiedo all'utente il suo colore preferito

--- assets/JavaScript/main.js
+++ assets/JavaScript/main.js
@@ -31,8 +31,11 @@ Infine scriviamo sulla pagina "la tua password insicurissima è: nomecognomecolo
 
 let nome = prompt("Nome: ");
 let cognome = prompt("Cognome: ");
+let colore = prompt("Colore preferito: ");
 
 console.log(nome);
 console.log(cognome);
+console.log(colore);
 document.getElementById('nome').innerHTML = `Il tuo nome è ${nome}`;
 document.getElementById('cognome').innerHTML = `Il tuo cognome è ${cognome}`;
+document.getElementById('colore').innerHTML = `Il tuo colore preferito è ${colore}`;
