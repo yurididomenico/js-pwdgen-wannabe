5e50fa283a645134564120307021aeaa3e008e08
yurididomenico <yurididomenico93@gmail.com>
2022-10-18 13:46:32 +0200

    Set Workspace

---
+++ assets/JavaScript/main.js
@@ -0,0 +1,9 @@
+/*
+
+Chiedi all’utente il suo nome,
+poi chiedi il suo cognome,
+poi chiedi il suo colore preferito
+aggiungiamo alla fine un numero
+Infine scriviamo sulla pagina "la tua password insicurissima è: nomecognomecolorepreferito21"
+
+*/
